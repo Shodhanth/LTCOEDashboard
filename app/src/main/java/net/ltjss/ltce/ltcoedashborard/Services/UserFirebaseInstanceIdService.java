ef2e75db7a635e3b5593304db94900ff70ed5707
Peng0x539 <vtewari26@gmail.com>
2016-12-20 17:40:33 +0530

    Add push notification :smirk:

---
+++ app/src/main/java/net/ltjss/ltce/ltcoedashborard/Services/UserFirebaseInstanceIdService.java
@@ -0,0 +1,20 @@
+package net.ltjss.ltce.ltcoedashborard.Services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by a0_ on 20/12/16.
+ */
+public class UserFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = UserFirebaseInstanceIdService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "onTokenRefresh: " + token);
+    }
+}
---
+++ app/src/main/java/net/ltjss/ltce/ltcoedashborard/Services/UserFirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package net.ltjss.ltce.ltcoedashborard.Services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import net.ltjss.ltce.ltcoedashborard.LoginActivity;
+import net.ltjss.ltce.ltcoedashborard.R;
+
+/**
+ * Created by a0_ on 20/12/16.
+ */
+public class UserFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle("LTCOE Dashboard Notification");
+        notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notificationBuilder.setContentIntent(pendingIntent);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, notificationBuilder.build());
+    }
+}
